df1d7f0cdded626fea880bd5e733ecb9819b59db
ArchUsr64 <anshulusr@gmail.com>
2022-05-21 11:41:00 +0530

    Added script to disable builtin input on external keyboard

---
+++ scripts/py/disable_builtin_input.py
@@ -0,0 +1,18 @@
+impor: subprocess
+import os
+import time
+
+while (True):
+    output = ""
+    disabled = False
+    try:
+        output = subprocess.check_output("swaymsg -t get_inputs | grep 12x4_Nugget", shell = True);
+    except Exception:
+        output = ""
+    if (output == ""):
+        os.system("swaymsg input 1:1:AT_Translated_Set_2_keyboard events enabled")
+        os.system("swaymsg input 1267:12631:ASUE1409:00_04F3:3157_Touchpad events enabled")
+    else:
+        os.system("swaymsg input 1:1:AT_Translated_Set_2_keyboard events disabled")
+        os.system("swaymsg input 1267:12631:ASUE1409:00_04F3:3157_Touchpad events disabled")
+    time.sleep(1)

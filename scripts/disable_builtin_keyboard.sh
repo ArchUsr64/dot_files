a9fd5a0f11e6f93433a676548fbd22e33647fedb
ArchUsr64 <anshulusr@gmail.com>
2023-06-30 15:14:24 +0530

    ported keyboard disable script to bash

---
+++ scripts/disable_builtin_keyboard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+external_keyboard_connected=false
+internal_input_enabled=true
+
+while true; do
+    command_output=$(swaymsg -t get_inputs | grep ArchUsr64_Rusty_Egboard)
+    if [[ -z $command_output ]]; then
+        external_keyboard_connected=false
+    else
+        external_keyboard_connected=true
+    fi
+
+    if [[ $external_keyboard_connected == true && $internal_input_enabled == true ]]; then
+        internal_input_enabled=false
+        swaymsg input 1:1:AT_Translated_Set_2_keyboard events disabled
+    elif [[ $external_keyboard_connected == false && $internal_input_enabled == false ]]; then
+        internal_input_enabled=true
+        swaymsg input 1:1:AT_Translated_Set_2_keyboard events enabled
+    fi
+
+    sleep 1
+done
+
